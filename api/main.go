6d5be9ba7fdce1a211f20350bc4a42bb3bad6068
Samarth Namdeo <namdeosamarth@gmail.com>
2024-07-18 20:52:50 +0530

    Shorten.go and resolve.go almost completed

--- api/main.go
+++ api/main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
+	"github.com/samarth-namdeo/SwiftLink/routes"
 )
 
-func setupRoutes(app *fiber.App){
-	app.Get("/url" , routes.ResolveURL)
-	app.Post("/api/v1" , routes.ShortenURL)
+func setupRoutes(app *fiber.App) {
+	app.Get("/url", routes.ResolveURL)
+	app.Post("/api/v1", routes.ShortenURL)
 }
 
 func main() {
@@ -22,12 +24,10 @@ func main() {
 	}
 
 	app := fiber.New()
-	
-	app.Use(logger.New())
 
+	app.Use(logger.New())
 
 	setupRoutes(app)
 
-	log.Fatal(app.Listen(os.Getenv("APP_PORT"))
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
 }
-
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,9 +1,15 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	goValidator "github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/samarth-namdeo/SwiftLink/database"
 	"github.com/samarth-namdeo/SwiftLink/helpers"
 )
 
@@ -32,6 +38,25 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	// implement rate limiting
 
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			// rate limit exceeded
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
+				"error":            "Rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+
+	}
+
 	// check if the URL is valid
 	if !goValidator.IsURL(req.URL) {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -41,13 +66,62 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	// check for domain error
 	if !helpers.RemoveDomainError(req.URL) {
-		return c.Status(fiber.StaturServiceUnavailable).JSON(fiber.Map{
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
 			"error": "URL is not allowed",
 		})
 	}
 
 	// enforce https, ssl
-
 	req.URL = helpers.EnforceHTTPS(req.URL)
+
+	var id string
+
+	if req.CustomShort == "" {
+		id = uuid.New().String()[:6] // generates a random 6 character string
+	} else {
+		id = req.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "Short URL already exists",
+		})
+	}
+
+	if req.Expiry == 0 {
+		req.Expiry = 24 * time.Hour
+	}
+
+	err = r.Set(database.Ctx, id, req.URL, req.Expiry*3600*time.Second).Err()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Cannot connect to server",
+		})
+	}
+
+	resp := response{
+		URL:             req.URL,
+		CustomShort:     "",
+		Expiry:          req.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
+
+	// Decrease the rate limit
+	r2.Decr(database.Ctx, c.IP())
+
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(resp)
 
 }
